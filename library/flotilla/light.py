6c43c8968a49f0c9dd6bd3e7aca52f0208979a4a
Philip Howard <phil@gadgetoid.com>
2016-05-20 15:37:59 +0100

    Update light.py

--- library/flotilla/light.py
+++ library/flotilla/light.py
@@ -9,3 +9,9 @@ class Light(Module):
         if len(self.data) > 0:
             return int(self.data[0])
         return 0
+
+    @property
+    def lux(self):
+        if len(self.data) > 2:
+            return int(self.data[2])
+        return 0
